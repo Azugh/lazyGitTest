b8c060517412c3545989ec3e55a7d839a56c8fdb
Azugh <lybimov.egor@gmail.com>
2024-11-07 18:18:04 +0300

    first commit

---
+++ src/main/java/accessmodifiers/App.java
@@ -0,0 +1,15 @@
+package accessmodifiers;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// TODO
+
+public class App {
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+
+    sc.close();
+  }
+}
